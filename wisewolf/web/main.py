ec01f9c13e065cd1a6d3d7b5ed43db0f40c955c2
Holo <rlee@cyrusian.com>
2014-07-15 15:03:56 +0000

    make user page search by tag & by title

--- wisewolf/web/main.py
+++ wisewolf/web/main.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 from wisewolf.web.redis_session import RedisSessionInterface
 from wisewolf.web import app
 from wisewolf.db_pool import Mongo_Wisewolf, redis_RoomSession, Psql_Cursor
+import wisewolf.web.search_room as search_room
 import binascii
 import time
 import wisewolf.web.views as views
@@ -102,32 +103,7 @@ def get_room_info():
 
 @app.route('/get_room_list', methods=['POST'])
 def get_room_list():
-	search_request={}
-	try:
-		search_request= request.get_json(force=True)
-	except:
-		return '{}'
-	r= redis_RoomSession
-	room_list= r.keys()
-	room_list.sort(reverse= True)
-	result={}
-	result['room_info']=[]
-	result_counter=0
-	for key in [x.decode() for x in room_list]:
-		if "support" in key:
-			continue
-		if result_counter>=int(search_request['seq'])+20:
-			break
-		room= json.loads(r.get(key).decode("utf-8"))
-		if Markup.escape(search_request["keyword"]) in room["room_title"]:
-			result_counter+=1
-			if result_counter>= search_request['seq']:
-				try:
-					result['room_info'].append({"key":key, "title":room["room_title"],"cur_participants":room["cur_participants"],
-"max_participants":(lambda x: '*' if x=='' else x)(room["max_participants"])})
-				except:
-					result['room_info'].append(room_info= {"key":"error", "title":"error", "max_participants":"error", "cur_participants":"error"})
-	return json.dumps(result)
+	return search_room.search_room(request)
 	
 @app.route('/chatting/<path:path>', methods=['POST','GET'])
 def chattingroom(path):
