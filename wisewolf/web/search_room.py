c7c9c4eaa163980eafd60aefe069f80aaabe68fc
Holo <rlee@cyrusian.com>
2014-07-23 11:26:08 +0000

    fixed tag search code to work with changed tag structure

--- wisewolf/web/search_room.py
+++ wisewolf/web/search_room.py
@@ -2,6 +2,8 @@ from wisewolf.db_pool import redis_RoomSession, Mongo_Wisewolf
 import json
 from flask import Markup
 
+from re import escape as re_escape
+
 def search_room(request):
 	search_request={}
 	try:
@@ -33,14 +35,30 @@ def search_room(request):
 				if result_counter> req_seq:
 					try:
 						result['room_info'].append({"key":key, "title":room["room_title"],"cur_participants":room["cur_participants"],
-	"max_participants":(lambda x: '*' if x=='' else x)(room["max_participants"])})
+"max_participants":(lambda x: '*' if x=='' else x)(room["max_participants"])})
 					except:
-						result['room_info'].append(room_info= {"key":"error", "title":"error", "max_participants":"error", "cur_participants":"error"})
-		return json.dumps(result)
-	else:
-		room_collection= Mongo_Wisewolf.rooms
-		result_list= room_collection.find({"tags.tag":keyword}).sort("_id",-1).skip(req_seq).limit(20)
+						result['room_info'].append({"key":"error", "title":"error", "max_participants":"error", "cur_participants":"error"})
+
+	elif search_mode["search_by"]== "tag":
+		result_list= get_list_from_mongo(keyword, search_mode, req_seq)
 		for room in result_list:
-			result['room_info'].append({"key":room["room_seq"], "title":room["room_title"],"cur_participants":"0",
-	"max_participants":(lambda x: '*' if x=='' else x)(room["max_participants"])})
-		return json.dumps(result)
+			try:
+				result['room_info'].append({"key":room["room_seq"], "title":room["room_title"],"cur_participants":"0",
+"max_participants":(lambda x: '*' if x=='' else x)(room["max_participants"])})
+			except:
+				result['room_info'].append({"key":"error", "title":"error", "max_participants":"error", "cur_participants":"error"})
+	return json.dumps(result)
+
+def get_list_from_mongo(keyword, search_mode, req_seq):
+	#room_collection= Mongo_Wisewolf.rooms
+	#return room_collection.find({"tags.tag":keyword}).sort("_id",-1).skip(req_seq).limit(20)#room_collection.find({"room_title":{"$regex":"(?i).*"+re_escape(keyword)+".*"}}).sort("_id",-1).skip(req_seq).limit(20)
+
+	try:
+		room_list= Mongo_Wisewolf.tags.find_one({"tag":keyword},{"room_list.room_seq":1})["room_list"]
+	except TypeError as e:
+		return []
+
+	room_seq_list=[]
+	for room in room_list:
+		room_seq_list.append(room["room_seq"])
+	return Mongo_Wisewolf.rooms.find({"room_seq":{"$in":room_seq_list}})
