ac60c654e41a3f4d2009caadb15536a4139fc5b2
Holo <rlee@cyrusian.com>
2014-10-17 16:02:45 +0000

    mongo db connection config changed write concern set 2 and timeout set 200ms. using repl set

--- wisewolf/db_pool.py
+++ wisewolf/db_pool.py
@@ -10,7 +10,7 @@ import psycopg2
 redis_UserSession= Redis(connection_pool= ConnectionPool(host=DB_HOST, db=REDIS_INDEX['user_sessions']))
 redis_RoomSession= Redis(connection_pool= ConnectionPool(host=DB_HOST, db=REDIS_INDEX['chatting_rooms']))
 
-Mongo_Wisewolf= Connection(['165.194.104.192','165.194.104.192:40001','165.194.104.192:40002']).wisewolf
+Mongo_Wisewolf= Connection(['165.194.104.192','165.194.104.192:40001','165.194.104.192:40002'], w=2, wtimeout= 200).wisewolf
 Mongo_Wisewolf.authenticate(MONGO_AUTHENTICATE["id"],MONGO_AUTHENTICATE["passwd"])
 
 con= psycopg2.connect(database='wisewolf', user=PSQL_AUTHENTICATE["user"], password=PSQL_AUTHENTICATE["passwd"], host=DB_HOST)
